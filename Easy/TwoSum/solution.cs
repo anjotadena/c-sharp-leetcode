46076efec4b4efc24cca3c7ce7333c668b5f0809
Anjo Tadena <tadena.anjo@gmail.com>
2023-02-26 21:44:02 +0800

    [easy-001] - Two sum add brute force solution

--- Easy/TwoSum/solution.cs
+++ Easy/TwoSum/solution.cs
@@ -1,17 +1,47 @@
-public class Solution {
-    public int[] TwoSum(int[] nums, int target) {
-        Dictionary<int, int> value = new();
-
+public class Solution
+{
+    public int[] TwoSum(int[] nums, int target)
+    {
         if (nums == null || nums.Length < 2)
         {
             return Array.Empty<int>();
         }
 
+        // TwoSumBruteForce(nums, target);
+
+        TwoSumOptimize(nums, target);
+    }
+
+    // Time complexity O(n^2)
+    // Space complexity O(1)
+    public int[] TwoSumBruteForce(int[] nums, int target)
+    {
+        for (int i = 0; i < nums.Length; i++)
+        {
+            for (int j = 0; j < nums.Length; j++)
+            {
+                if ((nums[i] + nums[j]) == target)
+                {
+                    return new int[] { i, j };
+                }
+            }
+        }
+
+        return Array.Empty<int>();
+    }
+
+    // Time complexity O(n)
+    // Space complecity O(n)
+    public int[] TwoSumOptimize(int[] nums, int target)
+    {
+        // optimize way
+        Dictionary<int, int> value = new();
+
         for (int i = 0; i < nums.Length; i++)
         {
             if (value.TryGetValue(target - nums[i], out int index))
             {
-                return new[] { index, i};
+                return new[] { index, i };
             }
 
             value[nums[i]] = i;
